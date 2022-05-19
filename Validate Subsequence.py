f565806c8f23c6458eafb7550c954512ec77102a
Md. Irtiza Hossain <mdirtiza.hossain1999@gmail.com>
2022-05-19 10:18:43 +0600

    2

--- Validate Subsequence.py
+++ Validate Subsequence.py
@@ -1,1 +1,17 @@
 # Validate Subsequence
+# Time O(n) Space O(1)
+
+arr1 = [5, 1, 22, 25, 6, -1, 8, 10]
+targetArr = [1, 6, -1, 10]
+out = []
+pointer2 = 0
+len2 = len(targetArr)
+
+for i in range(len(arr1)):
+    if pointer2 == len2:
+        break
+    if arr1[i] == targetArr[pointer2]:
+        out.append(targetArr[pointer2])
+        pointer2 += 1
+
+print(pointer2 == len2)
